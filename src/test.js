120babe005dd80b0cb83ed569f2943db91ec8a30
Tomas <gonzalez.tommy2001@gmail.com>
2022-07-22 08:54:22 -0400

    Dependencies update

--- src/test.js
+++ src/test.js
@@ -14,7 +14,7 @@ module.exports =  {
     if (!fs.existsSync(nodeModulesPath+'/prettier')) throw InternalError(`Uknown prettier path`);
 
     if (!shell.which('jest')) {
-      const packageName = "jest@24.8.0";
+      const packageName = "jest@28.1.3";
       throw TestingError(`🚫 You need to have ${packageName} installed to run test the exercises, run $ npm i ${packageName} -g`);
     }
 
